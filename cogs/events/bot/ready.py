e7530145b9dc1734a7458ae81a59df505c80423b
TaylorsTheorem <erdem.akar@icloud.com>
2025-03-11 23:45:47 +0100

    Fix Github Actions with test on_ready message

--- cogs/events/bot/ready.py
+++ cogs/events/bot/ready.py
@@ -15,4 +15,4 @@ class Ready(ipy.Extension):
         guild = await self.bot.fetch_guild(GUILD_ID, force=True)
         await guild.gateway_chunk(wait=True, presences=True)
 
-        print('Bot now launched!')
+        print('Bot now launched! Ready for actions!')
