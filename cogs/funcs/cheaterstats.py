2d9c5b3313412538ec1c3aac110bb75e300fab9a
TaylorsTheorem <erdem.akar@icloud.com>
2025-04-14 18:39:51 +0200

    Fix URL again

--- cogs/funcs/cheaterstats.py
+++ cogs/funcs/cheaterstats.py
@@ -4,7 +4,7 @@ import requests, os
 from datetime import datetime
 from config import LOGO_URL
 
-url = 'http://cheaterstats.xyz/api/checkUser.php?'
+url = 'https://api.cheaterstats.cc/api/checkUser?'
 
 api_token = os.getenv('CHEATERSTATS_TOKEN')
 headers = {
